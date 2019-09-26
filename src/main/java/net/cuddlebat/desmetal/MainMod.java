22d54f877dcb6a2e72e850e90f4540ef06dae0be
des-cuddlebat <app@cuddlebat.net>
2019-09-26 18:58:52 +0200

    I .. did something I guess

---
+++ src/main/java/net/cuddlebat/desmetal/MainMod.java
@@ -0,0 +1,12 @@
+package net.cuddlebat.desmetal;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MainMod implements ModInitializer
+{
+	@Override
+	public void onInitialize()
+	{
+		System.out.println("Person below banana");
+	}
+}
